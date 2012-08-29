3ac649be5eed7137bbda71cd55b9bf1f4ed5737c
Michael Stewart <michael_stewart+gitdev@vt.edu>
2012-08-29 09:06:22 -0400

    update logging to work better

--- app/controllers/log_service_controller.rb
+++ app/controllers/log_service_controller.rb
@@ -11,16 +11,19 @@ class LogServiceController < ActionController::Base
     puts "\n\n\n\n\n\n\n\n\n\n===========\n\n\n\n\n\n\n\n out log"
     failures = []
     params[:logs].each do |idx, log|
+      if params[:application]
+        log[:application_id] = Application.find_by_name(params[:application]).id
+      end
       @interaction_log = InteractionLog.new(log)
       if not @interaction_log.time
         @interaction_log.time = DateTime.now
       end
       if not @interaction_log.person_id
-        if cookies[:last_user_id]
-          @interaction_log.unknown_user_details = cookies[:last_user_id]
-        else
-          @interaction_log.unknown_user_details = request.remote_ip
-        end
+        # if cookies[:last_user_id]
+        #   @interaction_log.unknown_user_details = "last_user_id: " + cookies[:last_user_id]
+        # else
+          @interaction_log.unknown_user_details = "request.remote_ip: " + request.remote_ip
+        # end
       end
       if not @interaction_log.save
         failures.push(@interaction_log.details)
