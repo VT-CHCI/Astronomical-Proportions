6d35ee1b8332b422ef8ccdce9021edbb9939fdab
Michael Stewart <michael_stewart+gitdev@vt.edu>
2012-09-03 13:03:14 -0400

    minor bug fixes on person model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < Devise::SessionsController
 	def create
     super
     if person_signed_in?
-    	cookies.permanent[:last_user_id] = current_person.id
+    	cookies.permanent[:last_person_id] = current_person.id
     end
   end
 end
