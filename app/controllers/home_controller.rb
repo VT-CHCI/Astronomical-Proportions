3c12f7769efb8ccac2133bf0500734a63cc20015
Michael Stewart <michael_stewart+gitdev@vt.edu>
2012-11-09 05:17:49 -0500

    code for pilot!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,11 @@ class HomeController < ApplicationController
   def index
   	@people = Person.all
   	@itemsDict = {}
-
+    i = 0
   	for person in @people
       items = Item.where(:person_id => person.id)
       if items.length > 0
-  		  @itemsDict[person] = items
+  		  @itemsDict[i+=1] = items
       end
   	end
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :person
   has_and_belongs_to_many:courses
+  validates :filename, :presence => true
   mount_uploader :filename, ImageUploader
 end
